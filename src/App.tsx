36463ce92c3a903964baf2004a9925f60888522a
Hoseop Jeong <hosap4ik@gmail.com>
2022-06-23 21:47:02 +0200

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+ const App=()=>{
+    return (
+    <div className="bg-blue-500">
+    <h1>Create React App from Scratch Using Typescript..!!</h1>
+    </div>
+    )
+}
+export default App;
