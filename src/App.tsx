47a2e1b0e6c8b2df68dcc3974610619015f8f5b8
Hoseop Jeong <hosap4ik@gmail.com>
2022-06-27 19:33:23 +0200

    did some css change

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,23 @@ export default App;
 const SearchPanel=()=>{
     const searchElementRef = useRef(null as any);
     const search =(e:SyntheticEvent)=>{
-        console.log(searchElementRef.current.value);
+        const searchValue = searchElementRef.current.value;
+        if(typeof searchValue!=="string"){
+            alert("only text allowed");
+        }else{
+            regexValidate(searchValue)
+            ?alert("No special characters are allowed")
+            :alert("good!")
+        }
         e.preventDefault();
+    }
+    const regexValidate = (value:string)=>{
+        const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        return specialChars.test(value);
     }
     return(
         <section className="text-center absoluteCenter">
-            <h1>Wanna travel somewhere?</h1>
+            <h1 className="text-white text-lg uppercase mb-5">Wanna travel somewhere?</h1>
             <form action="/" method="GET" onSubmit={e=>search(e)}>
                 <input type="text" placeholder="SEARCH" ref={searchElementRef}/>
             </form>
