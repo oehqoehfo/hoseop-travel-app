ccb00e6a8baa2f3e4df487dbaa8cdfbc6805e1d1
Hoseop Jeong <hosap4ik@gmail.com>
2022-07-05 18:50:42 +0200

    modified result style

--- src/Result.tsx
+++ src/Result.tsx
@@ -4,10 +4,17 @@ interface resultProps{
 }
 const Result=({result}:resultProps)=>{
     return(
-        <div>
+        <div id="Result" className="flex w-100 justify-around">
             {result.map((item,index)=>{
                 console.log(item);
-                return <div key={"item-"+item+index}>{item[1]['placeName']}</div>
+                //return /<div className="card" key={"item-"+item+index}>{item[1]['placeName']}</div>
+                return (
+                    <div key={"item-"+item+index} className="item w-1/4 text-center text-white">
+                        <div>
+                            <p>{item[1]['placeName']}</p>
+                        </div>
+                    </div> 
+                )   
             })}
         </div>
     )
